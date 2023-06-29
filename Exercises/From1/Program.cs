9d0eb7d2efcf4f24c87cb0d43065eaf85e07f914
Dexstur <chuks89@live.com>
2023-06-29 16:36:00 +0100

    initial commit

---
+++ Exercises/From1/Program.cs
@@ -0,0 +1,8 @@
+﻿using From1;
+
+Student student1 = new Student();
+Student student2 = new Student("Mary");
+Student student3 = new Student("James", "james@decagon.dev", "08912335246");
+Console.WriteLine(student1.Name);
+Console.WriteLine(Student.Count);
+Student.GetInfo(student3);
---
+++ Exercises/From1/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace From1
+{
+    public class Student
+    {
+        public string Name { get; set; }    
+        public string ?Course { get; set; }
+        public string ?University { get; set; }
+        public string ?Email { get; set; }
+        public string ?PhoneNo { get; set; }
+        public static int Count { get; private set; } = 0;
+        public Student() {
+            Count++;
+            Name = "student" + Count;
+        }
+        public Student(string name)
+        {
+            Name = name;
+            Count++;
+        }
+        public Student(string name, string email, string phoneNo)
+        {
+            Name = name;
+            Email = email;
+            PhoneNo = phoneNo;
+            Count++;
+        }
+        public static void GetInfo(Student student)
+        {
+            Console.WriteLine($"Name: { student.Name}");
+            if(student.Email != null) Console.WriteLine($"Email: {student.Email}");
+            if(student.PhoneNo != null) Console.WriteLine($"Phone Number: {student.PhoneNo}");
+            if(student.University != null)Console.WriteLine($"Institution: {student.University}");
+            if(student.Course != null) Console.WriteLine($"Course: {student.Course}");
+        }
+
+    }
+}
